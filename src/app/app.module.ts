ed1952be090cbcd7236aeda968409fde184734b7
Bethel Eyo <betheleyo@gmail.com>
2024-04-11 21:35:50 +0100

    major commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 import { CatalogComponent } from './catalog/catalog.component';
-import { SiteHeaderComponent } from './site-header/site-header.component';
+import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
@@ -13,12 +18,11 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     HomeComponent,
     CatalogComponent,
     SiteHeaderComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, UserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
